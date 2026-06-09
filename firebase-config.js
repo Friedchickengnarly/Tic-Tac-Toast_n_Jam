4fd8cdea392a112f275c9897c6de859f0e0542f2
Friedchickengnarly <ivan.soto.jasso81@gmail.com>
2026-06-09 17:26:25 -0600

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,21 @@
-// Paste your Firebase web app config here after creating a Firebase project.
-// This file is safe to publish; Firebase web API keys identify a project, not a secret.
-window.TICTACTOAST_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCi_6DFzFANAPqY-3nfbV7aJ0oy0DuZQZs",
+  authDomain: "tictactoast.firebaseapp.com",
+  projectId: "tictactoast",
+  storageBucket: "tictactoast.firebasestorage.app",
+  messagingSenderId: "1051170393793",
+  appId: "1:1051170393793:web:48405c6470632735188e40",
+  measurementId: "G-J37JWNZ69E"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
